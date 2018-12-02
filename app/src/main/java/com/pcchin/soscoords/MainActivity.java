b3b03fed224708b8ba9365a06e0ecc34c378aeda
PC Chin <chin.pengcheng@gmail.com>
2018-12-02 23:17:25 +0800

    Initial commit

---
+++ app/src/main/java/com/pcchin/soscoords/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pcchin.soscoords;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Main Format: String names contain underscores, ids use camelCase, functions use
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
