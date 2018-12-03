eeda48d92a58de6a1bee8ac3c376c75746776edf
PC Chin <chin.pengcheng@gmail.com>
2018-12-03 18:46:06 +0800

    Frontend UI Complete (3)

--- app/src/main/java/com/pcchin/soscoords/MainActivity.java
+++ app/src/main/java/com/pcchin/soscoords/MainActivity.java
@@ -3,6 +3,7 @@ package com.pcchin.soscoords;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Main Format: String names contain underscores, ids use camelCase, functions use
@@ -13,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // TODO: Complete function to store info
+    public void storeInfo(String name, String value) { }
+
+    // TODO: Complete function to read info
+    public <T> ArrayList<T> readInfo(String name) {
+        ArrayList<T> response = new ArrayList<>();
+        return response;
     }
 }
