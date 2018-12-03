c142ceb9c1bd3534cd129d2fbea2a04d2862b33a
PC Chin <chin.pengcheng@gmail.com>
2018-12-03 17:35:29 +0800

    Frontend UI Complete

--- app/src/main/java/com/pcchin/soscoords/MainActivity.java
+++ app/src/main/java/com/pcchin/soscoords/MainActivity.java
@@ -3,6 +3,8 @@ package com.pcchin.soscoords;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 // Main Format: String names contain underscores, ids use camelCase, functions use
 
 public class MainActivity extends AppCompatActivity {
