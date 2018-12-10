5fce8495b92101bb2e9caa27da260162195351b8
PC Chin <chin.pengcheng@gmail.com>
2018-12-10 15:41:20 +0800

    Started on background application

--- app/src/main/java/com/pcchin/soscoords/BackgroundSmsMonitor.java
+++ app/src/main/java/com/pcchin/soscoords/BackgroundSmsMonitor.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.widget.Toast;
 
 import com.pcchin.soscoords.contactlist.ContactListDatabase;
 
@@ -13,45 +14,45 @@ import java.util.List;
 
 public class BackgroundSmsMonitor extends IntentService {
 
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public BackgroundSmsMonitor(String name) {
-        super(name);
+    public BackgroundSmsMonitor() {
+        super("BackgroundSmsMonitor");
     }
 
     @Override
-        protected void onHandleIntent(Intent workIntent) {
-            // TODO: SEE BELOW
-            // Add event notifier for new sms
-            // Check checkbox status
-            // Check sender matches number in list
-            // Check SMS content matches code
-            // Send reply (Reply 0 to exit, 1 for gps coordinates, 2 for ringing phone)
-            // Wait for 1st reply
-            // If reply is either 1 or two, do corresponding action
-            // Update database when MainEvent calls for
-            // When booted, go to step 2
-        }
+    protected void onHandleIntent(Intent workIntent) {
+        // TODO: SEE BELOW
+        // USE EVERNOTE API OR FIREBASE JOB DISPATCHER
+        // Add event notifier for new sms
+        // Check checkbox status
+        // Check sender matches number in list
+        // Check SMS content matches code
+        // Send reply (Reply 0 to exit, 1 for gps coordinates, 2 for ringing phone)
+        // Wait for 1st reply
+        // If reply is either 1 or two, do corresponding action
+        // When booted, go to step 2
 
-        @NonNull
-        private List<String> updateNumberList() {
-            // Access database
-            ContactListDatabase contactListDatabase = Room.databaseBuilder(getApplicationContext(), ContactListDatabase.class, "current_contact_list").build();
-            List<String> updateNumberString = contactListDatabase.daoAccess().getAllContactsNum();
-            contactListDatabase.close();
+        // Set global variable to let main program know when app is running
+        // Restart/start database when any settings are changed
 
-            // Convert String back to array
-            List<String> responseArray = new ArrayList<>();
-            //Merge all arrays into one
-            for (int i=0; i < updateNumberString.size(); i++) {
-                // Convert current phone number stored in contacts (String to Array)
-                List<String> responseArrayCurrent = new ArrayList<>(Arrays.asList(updateNumberString.get(i).split("; ")));
-                // Append all values in current array to final array
-                responseArray.addAll(responseArrayCurrent);
-            }
-            return responseArray;
+        Toast.makeText(this, "Sms Coords started", Toast.LENGTH_SHORT).show();
+    }
+
+    @NonNull
+    private List<String> updateNumberList() {
+        // Access database
+        ContactListDatabase contactListDatabase = Room.databaseBuilder(this, ContactListDatabase.class, "current_contact_list").build();
+        List<String> updateNumberString = contactListDatabase.daoAccess().getAllContactsNum();
+        contactListDatabase.close();
+
+        // Convert String back to array
+        List<String> responseArray = new ArrayList<>();
+        //Merge all arrays into one
+        for (int i=0; i < updateNumberString.size(); i++) {
+            // Convert current phone number stored in contacts (String to Array)
+            List<String> responseArrayCurrent = new ArrayList<>(Arrays.asList(updateNumberString.get(i).split("; ")));
+            // Append all values in current array to final array
+            responseArray.addAll(responseArrayCurrent);
         }
+        return responseArray;
+    }
 }
